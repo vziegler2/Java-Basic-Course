9e20e129cb2f7cb0fc8fdad03be1684d8a816452
vziegler <vziegler@student.42wolfsburg.de>
2023-02-15 05:47:47 +0100

    Hello World

--- random-number/src/HelloClass.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class HelloClass {
-    public static void main(String[] args) {
-        var r = new Random();
-        System.out.println("Eine Zufallszahl: " + r.nextInt(100));
-    }
-}
